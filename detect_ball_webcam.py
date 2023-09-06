dc1364e7fa02e6d78dfe65050df8815734bd17ef
zimmeras <zimmeras@purdue.edu>
2023-09-05 22:38:05 -0400

    renamed file

--- detect_ball_webcam.py
+++ detect_ball_webcam.py
@@ -2,13 +2,10 @@ import cv2 as cv
 import numpy as np
 
 videoCapture = cv.VideoCapture(0)
-prevCircle = None
-dist = lambda x1, y1, x2, y2: (x1-x2)**2 + (y1-y2)**2
 
 while True:
     ret, frame = videoCapture.read()
     if not ret: break
-    cv.imshow("circles", frame)
 
     grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     blurFrame = cv.GaussianBlur(grayFrame, (17,17), 0)
@@ -18,11 +15,12 @@ while True:
     
     if circles is not None:
         circles = np.uint32(np.around(circles))
-        for i in circles[0, :]:
-            cv.circle(frame, (i[0], i[1]), 1, (0,100,100), 3)
-            cv.circle(frame, (i[0], i[1]), i[2], (255, 0, 255), 3)
+        cv.circle(frame, (circles[0,0,0], circles[0,0,1]), circles[0,0,2], (255, 0, 255), 3)
+        # for i in circles[0, :]:
+        #     cv.circle(frame, (i[0], i[1]), 1, (0,100,100), 3)
+        #     cv.circle(frame, (i[0], i[1]), i[2], (255, 0, 255), 3)
 
-    cv.imshow("circles", frame)
+    cv.imshow("frame with circles", frame)
     if cv.waitKey(1) & 0xFF == ord('q'): break
 
 videoCapture.release()
