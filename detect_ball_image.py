5404d56cf7e8ceeedea346a6045c9294f0a910cf
zimmeras <zimmeras@purdue.edu>
2023-09-15 11:40:13 -0400

    new stuff

--- detect_ball_image.py
+++ detect_ball_image.py
@@ -2,74 +2,53 @@ import cv2 as cv
 import numpy as np
 import time
 
+lower_pink = np.array([100, 20, 20])   # Lower bound for pink color in HSV
+upper_pink = np.array([170, 255, 255])  # Upper bound for pink color in HSV
+
 start = time.time()
-frame = cv.imread('ordered_pink_balls.png')
+frame = cv.imread('ball_lab.jpeg')
 # frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
 
 grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 blurFrame = cv.GaussianBlur(grayFrame, (17,17), 0)
 
-circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 5000, 
-                            param1=100, param2=40, minRadius=0, maxRadius=200)
+circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 50, 
+                            param1=160, param2=40, minRadius=0, maxRadius=300)
 
 if circles is not None:
     frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    lower_pink = np.array([100, 20, 20])   # Lower bound for pink color in HSV
-    upper_pink = np.array([170, 255, 255])  # Upper bound for pink color in HSV
+    circles = np.uint32(np.around(circles))
+    
+    for i in circles[0,:]:
+        x, y, r = i[0], i[1], i[2]
+        # print(f"X,Y,R: {x}, {y}, {r}")
+        cv.circle(frame, (x, y), r, (0, 255, 0), 3)
+        factor = 0.6
+        top = int(y - factor*r)
+        bot = int(y + factor*r)
+        left = int(x - factor*r)
+        right = int(x + factor*r)
 
-    x, y, r = int(circles[0,0,0]), int(circles[0,0,1]), circles[0,0,2]
-    factor = 0.5
-    top = int(y + factor*r)
-    bot = int(y - factor*r)
-    left = int(x - factor*r)
-    right = int(x + factor*r)
-    circle_subset_of_frame = frame[left:right, bot:top, :]
+        # circle_subset_of_frame = frame[top:bot, left:right, :]
+        # cv.imshow("subset color frame", circle_subset_of_frame)
+        # cv.waitKey()
 
-    x2 = x+20
-    y2 = y+20
-    print(frame[x,y,:])
-    print(frame[x2,y2,:])
+        circle_subset_of_frame = frame_hsv[top:bot, left:right, :]
+        pink_mask = cv.inRange(circle_subset_of_frame, lower_pink, upper_pink)
+        # cv.imshow("subset masked frame", pink_mask)
+        # cv.waitKey()
 
-    cv.imshow("subset color frame", circle_subset_of_frame)
-    cv.waitKey()
+        percent_pink = np.sum(pink_mask) /255 / pink_mask.size
+        pink_threshold = 0.75
 
-    pink_mask = cv.inRange(circle_subset_of_frame, lower_pink, upper_pink)
-    cv.imshow("subset masked frame", pink_mask)
-    cv.waitKey()
+        print(f"Percent pink: {percent_pink}")
 
-    percent_pink = np.sum(pink_mask) /255 / pink_mask.size
-    pink_threshold = 0.75
+        if percent_pink >= pink_threshold:
+            cv.circle(frame, (x, y), r, (0, 255, 0), 3)
 
-    print(f"Percent pink: {percent_pink}")
-
-    if percent_pink >= pink_threshold:
-        circles = np.uint32(np.around(circles))
-        cv.circle(frame, (circles[0,0,0], circles[0,0,1]), circles[0,0,2], (0, 255, 0), 3)
-
-    end = time.time()
-    print(f"time: {end-start}")
-
-    # cv.imshow("pink frame", pink_mask)
-    # cv.waitKey()
-
-    # maybe
-    # kernel = np.ones((5, 5), np.uint8)
-    # pink_mask = cv.erode(pink_mask, kernel, iterations=1)
-    # pink_mask = cv.dilate(pink_mask, kernel, iterations=1)
-
-    # cv.imshow("pink frame", pink_mask)
-    # cv.waitKey()
-
-    # contours, _ = cv.findContours(pink_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    # x, y = circles[0,0,0], circles[0,0,1]
-    # for contour in contours:
-    #     if cv.pointPolygonTest(contour, (x, y), False) >= 0:
-    #         circles = np.uint32(np.around(circles))
-    #         cv.circle(frame, (circles[0,0,0], circles[0,0,1]), circles[0,0,2], (255, 0, 255), 3)
-    #         break
-
-cv.imshow("circles", frame)
 end = time.time()
 print(f"time: {end-start}")
+
+cv.imshow("circles", frame)
 cv.waitKey()
 cv.destroyAllWindows()
--- detect_ball_video.py
+++ detect_ball_video.py
@@ -2,14 +2,17 @@ import cv2 as cv
 import numpy as np
 import time
 
-video = cv.VideoCapture("ball60fps.mov")
+lower_pink = np.array([120, 50, 50])   # Lower bound for pink color in HSV
+upper_pink = np.array([170, 255, 255])  # Upper bound for pink color in HSV
+
+video = cv.VideoCapture("pink_ball.mov")
 
 times = []
 begin = time.time()
 
-# frame_width = int(video.get(3))
-# frame_height = int(video.get(4))
-# size = (frame_width, frame_height)
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
+size = (frame_width, frame_height)
 # out_video = cv.VideoWriter('ball60fps_detected.mp4', 
 #                          cv.VideoWriter_fourcc(*'mp4'),
 #                          60, size)
@@ -22,38 +25,38 @@ while True:
     grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     blurFrame = cv.GaussianBlur(grayFrame, (17,17), 0)
 
-    circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 5000, 
-                            param1=100, param2=40, minRadius=0, maxRadius=200)
+    # circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 5000, 
+    #                         param1=100, param2=35, minRadius=0, maxRadius=200)
+    circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 50, 
+                            param1=140, param2=30, minRadius=0, maxRadius=250)
     
     if circles is not None:
-        # now want to check if the ball color is correct
-        # hot pink is about BGR: 180, 105, 255
-        # normal pink is about BGR: 193, 182, 255
-        # I think it could be good to say if the colors r within either a certain range or
-        # percentage of the range, they r good to be the ball
         frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        lower_pink = np.array([140, 50, 50])   # Lower bound for pink color in HSV
-        upper_pink = np.array([170, 255, 255])  # Upper bound for pink color in HSV
-        pink_mask = cv.inRange(frame_hsv, lower_pink, upper_pink)
+        circles = np.uint32(np.around(circles))
 
-        # maybe
-        kernel = np.ones((5, 5), np.uint8)
-        pink_mask = cv.erode(pink_mask, kernel, iterations=1)
-        pink_mask = cv.dilate(pink_mask, kernel, iterations=1)
+        for i in circles[0,:]:
+            x, y, r = i[0], i[1], i[2]
+            # cv.circle(frame, (x, y), r, (0, 255, 0), 3)
+            factor = 0.6
+            top = int(y - factor*r)
+            bot = int(y + factor*r)
+            left = int(x - factor*r)
+            right = int(x + factor*r)
 
-        contours, _ = cv.findContours(pink_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        x, y = circles[0,0,0], circles[0,0,1]
-        for contour in contours:
-            if cv.pointPolygonTest(contour, (x, y), False) >= 0:
-                print(f"Pixel at ({x}, {y}) is pink.")
-                circles = np.uint32(np.around(circles))
-                cv.circle(frame, (circles[0,0,0], circles[0,0,1]), circles[0,0,2], (255, 0, 255), 3)
-                break
-            else:
-                print(f"Pixel at ({x}, {y}) is not pink.")
+            if top < 0 or left < 0 or bot > frame_height or right > frame_width:
+                continue
+
+            circle_subset_of_frame = frame_hsv[top:bot, left:right, :]
+            pink_mask = cv.inRange(circle_subset_of_frame, lower_pink, upper_pink)
+
+            percent_pink = np.sum(pink_mask) /255 / pink_mask.size
+            pink_threshold = 0.75
+
+            if percent_pink >= pink_threshold:
+                cv.circle(frame, (x, y), r, (0, 255, 0), 3)
 
     # out_video.write(frame)
-    cv.imshow("frame with circles", frame)
+    cv.imshow("frame with circle", frame)
     if cv.waitKey(1) & 0xFF == ord('q'): break
     
     end = time.time()
