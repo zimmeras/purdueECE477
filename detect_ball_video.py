295085a868c2e1f26cb6cec160792a9fb1f97e8f
zimmeras <zimmeras@purdue.edu>
2023-09-20 13:08:35 -0400

    newest video and webcam

--- detect_ball_video.py
+++ detect_ball_video.py
@@ -2,62 +2,44 @@ import cv2 as cv
 import numpy as np
 import time
 
-lower_pink = np.array([120, 50, 50])   # Lower bound for pink color in HSV
+lower_pink = np.array([140, 150, 30])   # Lower bound for pink color in HSV
 upper_pink = np.array([170, 255, 255])  # Upper bound for pink color in HSV
 
-video = cv.VideoCapture("pink_ball.mov")
+video = cv.VideoCapture("ball_hub.mov")
 
 times = []
 begin = time.time()
 
-frame_width = int(video.get(3))
-frame_height = int(video.get(4))
-size = (frame_width, frame_height)
-# out_video = cv.VideoWriter('ball60fps_detected.mp4', 
-#                          cv.VideoWriter_fourcc(*'mp4'),
-#                          60, size)
+# frame_width = int(video.get(3))
+# frame_height = int(video.get(4))
+# size = (frame_width, frame_height)
+# out_video = cv.VideoWriter('pink_ball2.mp4', cv.VideoWriter_fourcc(*'mp4v'), 60, size)
 
 while True:
     start = time.time()
     ret, frame = video.read()
     if not ret: break
 
-    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    blurFrame = cv.GaussianBlur(grayFrame, (17,17), 0)
+    frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    pink_mask = cv.inRange(frame_hsv, lower_pink, upper_pink)
+    # cv.imshow("frame with circle", pink_mask)
+    # cv.waitKey()
+
+    blurFrame = cv.GaussianBlur(pink_mask, (17,17), 0)
 
     # circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 5000, 
     #                         param1=100, param2=35, minRadius=0, maxRadius=200)
-    circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 50, 
+    circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 100, 
                             param1=140, param2=30, minRadius=0, maxRadius=250)
     
     if circles is not None:
-        frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         circles = np.uint32(np.around(circles))
-
         for i in circles[0,:]:
-            x, y, r = i[0], i[1], i[2]
-            # cv.circle(frame, (x, y), r, (0, 255, 0), 3)
-            factor = 0.6
-            top = int(y - factor*r)
-            bot = int(y + factor*r)
-            left = int(x - factor*r)
-            right = int(x + factor*r)
-
-            if top < 0 or left < 0 or bot > frame_height or right > frame_width:
-                continue
-
-            circle_subset_of_frame = frame_hsv[top:bot, left:right, :]
-            pink_mask = cv.inRange(circle_subset_of_frame, lower_pink, upper_pink)
-
-            percent_pink = np.sum(pink_mask) /255 / pink_mask.size
-            pink_threshold = 0.75
-
-            if percent_pink >= pink_threshold:
-                cv.circle(frame, (x, y), r, (0, 255, 0), 3)
+            cv.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 3)
 
     # out_video.write(frame)
-    # cv.imshow("frame with circle", frame)
-    # if cv.waitKey(1) & 0xFF == ord('q'): break
+    cv.imshow("frame with circle", frame)
+    if cv.waitKey(1) & 0xFF == ord('q'): break
     
     end = time.time()
     times.append(end - start)
@@ -69,6 +51,6 @@ print(f"FPS: {fps}")
 end = time.time()
 print(f"Video took: {end-begin} s")
 
-# video.release()
+video.release()
 # out_video.release()
 cv.destroyAllWindows()
--- detect_ball_webcam.py
+++ detect_ball_webcam.py
@@ -2,26 +2,43 @@ import cv2 as cv
 import numpy as np
 import time
 
-videoCapture = cv.VideoCapture(0)
+lower_pink = np.array([140, 150, 30])   # Lower bound for pink color in HSV
+upper_pink = np.array([170, 255, 255])  # Upper bound for pink color in HSV
+
+video = cv.VideoCapture(0)
 
 times = []
+begin = time.time()
+
+# frame_width = int(video.get(3))
+# frame_height = int(video.get(4))
+# size = (frame_width, frame_height)
+# out_video = cv.VideoWriter('pink_ball2.mp4', cv.VideoWriter_fourcc(*'mp4v'), 60, size)
 
 while True:
     start = time.time()
-    ret, frame = videoCapture.read()
+    ret, frame = video.read()
     if not ret: break
 
-    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    blurFrame = cv.GaussianBlur(grayFrame, (17,17), 0)
+    frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    pink_mask = cv.inRange(frame_hsv, lower_pink, upper_pink)
+    # cv.imshow("frame with circle", pink_mask)
+    # cv.waitKey()
 
-    circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 5000, 
-                            param1=100, param2=40, minRadius=2, maxRadius=100)
+    blurFrame = cv.GaussianBlur(pink_mask, (17,17), 0)
+
+    # circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 5000, 
+    #                         param1=100, param2=35, minRadius=0, maxRadius=200)
+    circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.4, 100, 
+                            param1=140, param2=30, minRadius=0, maxRadius=250)
     
     if circles is not None:
         circles = np.uint32(np.around(circles))
-        cv.circle(frame, (circles[0,0,0], circles[0,0,1]), circles[0,0,2], (255, 0, 255), 3)
+        for i in circles[0,:]:
+            cv.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 3)
 
-    cv.imshow("frame with circles", frame)
+    # out_video.write(frame)
+    cv.imshow("frame with circle", frame)
     if cv.waitKey(1) & 0xFF == ord('q'): break
     
     end = time.time()
@@ -31,5 +48,9 @@ avg = np.mean(times)
 fps = 1 / avg
 print(f"FPS: {fps}")
 
-videoCapture.release()
+end = time.time()
+print(f"Video took: {end-begin} s")
+
+video.release()
+# out_video.release()
 cv.destroyAllWindows()
