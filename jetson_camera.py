079e4c5c84771556265dbad7a1eb7bc0154a803d
Andrew Zimmerman <zimmeras@purdue.edu>
2023-10-06 13:47:57 -0400

    cameras works!

---
+++ jetson_camera.py
@@ -0,0 +1,28 @@
+import cv2 as cv 
+import numpy as np
+import time
+
+pipeline = (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)NV12, framerate=(fraction)120/1 ! "
+        "nvvidconv flip-method=2 ! "
+        "video/x-raw, width=(int)1280, height=(int)720, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! "
+        "appsink"
+)
+
+video = cv.VideoCapture(pipeline, cv.CAP_GSTREAMER)
+if not video.isOpened():
+    print("Camera not opened")
+
+while True:
+    ret, frame = video.read()
+    if not ret:
+        break
+    cv.imshow("Camera Feed", frame)
+    if cv.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video.release()
+cv.destroyAllWindows()
