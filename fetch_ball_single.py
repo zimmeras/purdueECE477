1e8fb0e07ee9607379e3b9599fe4fddb1060d3c3
Andrew Zimmerman <zimmeras@purdue.edu>
2023-11-13 13:49:44 -0500

    final

--- fetch_ball_single.py
+++ fetch_ball_single.py
@@ -8,17 +8,10 @@ dist_factor = 16.1
 c, r, s = 542.7405475880053, 370.96088657105605, 20.189048239895698
 
 # Calculate horizontal angle based on the camera's FOV
-alpha = math.radians((c - (frame_width / 2)) / (frame_width / 2) * (camera_fov_deg / 2))
+r = math.radians((c - (frame_width / 2)) / (frame_width / 2) * (camera_fov_deg / 2))
 
 # Calculate distance to the ball based on the apparent size
-D = dist_factor / s
+y = dist_factor / s
 
-# Calculate x and y coordinates on the ground plane
-x = D * math.tan(alpha)
-y = D
-
-# Calculate r, might just be alpha, or pi/2 - alpha
-r = alpha
-
-print(f"X: {x}, Y: {y}, R: {r}")
+print(f"Y: {y}, R: {r}")
 
